00be778a3b6c8abe247268cf020065f6ca1eeb6b
hmagph <hmagph@users.noreply.github.com>
2016-02-06 22:36:15 +0100

    Update getItems.php

--- getItems.php
+++ getItems.php
@@ -5,6 +5,7 @@ function RetrieveItems()
     //echo "\r\n**************************************";
     $application = getenv("VCAP_APPLICATION");
     echo "\r\napplication:" . $application;
+    $application_json = json_decode($application, true);
     $applicationURI = $application_json["application_uris"][0];
     echo "\r\napplicationURI:" . $applicationURI;
     $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
